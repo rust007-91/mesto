2d059ca9c9f6ffe2faa76bcc44a9edd59d0a5a0b
Ruslan Rotkin <rust007-91@yandex.ru>
2023-01-22 16:38:13 +0300

    сделана демореализация карточек чере js

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,54 @@ const editButtonElement = profileElement.querySelector('.profile__edit-button');
 let profileTitleElement = profileElement.querySelector('.profile__title');
 let profileDescElement = profileElement.querySelector('.profile__description');
 
+const elementsCardTemplate = document.querySelector('#elements__card').content;
+const elementsListElement = document.querySelector('.elements__list');
+
+    // Клонируем содержимое тега template
+const CardElement = elementsCardTemplate.querySelector('.elements__card').cloneNode(true);
+
+    // Наполняем содержимым
+    CardElement.querySelector('.elements__card-image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
+    CardElement.querySelector('.elements__card-image').alt = 'Архыз';
+    CardElement.querySelector('.elements__title').textContent = 'Архыз';
+
+    const CardElement = elementsCardTemplate.querySelector('.elements__card').cloneNode(true);
+
+    // Наполняем содержимым
+    CardElement.querySelector('.elements__card-image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg';
+    CardElement.querySelector('.elements__card-image').alt = 'Челябинская область';
+    CardElement.querySelector('.elements__title').textContent = 'Челябинская область';
+
+    // Добавляем элемент на страницу
+    elementsListElement.append(CardElement);
+
+    // Добавление карточек
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
 
 const openForm = function () {
     popupElement.classList.add('popup_opened');
